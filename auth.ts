c43f07b264c2dba103a95efdaf8f8d3051033148
Amirhossein Aminnegareshi <108186011+amirhoprogrammer@users.noreply.github.com>
2025-12-31 21:43:30 +0330

    add button and setup auth js and ui

---
+++ auth.ts
@@ -0,0 +1,27 @@
+// import NextAuth from "next-auth";
+
+// export const { handlers, signIn, signOut, auth } = NextAuth({
+//   providers: [],
+// });
+import NextAuth from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+// // Your own logic for dealing with plaintext password strings; be careful!
+// import { saltAndHashPassword } from "@/utils/password";
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  providers: [
+    Credentials({
+      // You can specify which fields should be submitted, by adding keys to the `credentials` object.
+      // e.g. domain, username, password, 2FA token, etc.
+      credentials: {
+        username: { label: "Username" },
+        // email: {label : 'Email' , type: ''},
+        password: { label: "Password", type: "text" },
+      },
+      authorize: async (credentials) => {
+        console.log({ credentials });
+        return null;
+      },
+    }),
+  ],
+});
